348a03d4b4140fd9b18eea5e0d824bab0b04718b
souobbill <bsouob@gmail.com>
2021-05-27 09:43:06 +0100

    ajout de la configuration du port

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={logo} className="App-logo" alt="WouriTech" />
+
+
+
+        <p className = "P">
+           <code>WouriTech</code> L'Excellence.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
