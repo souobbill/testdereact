7be7066bd0439f4b4238efeb44a82f95d6bbf8c7
unknown <bsouob@gmail.com>
2021-08-14 02:04:09 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './assets/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="WouriTech" />
+
+
+
+        <p className = "P">
+           <code>WouriTech</code> L'Excellence.
+          
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
